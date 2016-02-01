041a2dc2bb848a5c2019a11a2232771da1e4dc82
jyu197 <jyu197@gmail.com>
2016-02-01 01:26:13 -0500

    Update 01_HelloWorld

--- 01_HelloWorld/src/Picture.java
+++ 01_HelloWorld/src/Picture.java
@@ -16,8 +16,8 @@ public class Picture {
         System.out.println("*********");
         System.out.println("**********");
         */
-    	for(int i = 1; i <= 10; i++){
-    		for(int j = 1; j <= i; j++){
+    	for (int i = 1; i <= 10; i++) {
+    		for (int j = 1; j <= i; j++) {
     			System.out.print("*");
     		}
     		System.out.println();
